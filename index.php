f3b3bf4617be15ec88418b4ee9550883069d9cf9
Max <serranocristian9@gmail.com>
2020-11-13 14:33:33 -0400

    Chore: Testing with original xml

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ const KEY_DGI = 'CERT_DGI_EFACTURA';
 $xmlObj = new XMLController();
 $encryptObj = new CryptController();
 
-$xml = $xmlObj->loadXml('assets/file.xml');
+$xml = $xmlObj->loadXml('firmedXml/original_edited.xml');
 $xml->createElementNS("http://cfe.dgi.gub.uy", "ns0");
 $xml = $encryptObj->encrypt($xml, 'assets/mycert.pem', KEY_DGI);
 $xmlObj->saveXml($xml);
