50033c4aa8978bd861b9b3bf8797af1c19953316
Max <serranocristian9@gmail.com>
2020-12-10 08:48:18 -0400

    Chore: Removed useless code

--- controllers/WsSoapClientController.php
+++ controllers/WsSoapClientController.php
@@ -3,8 +3,6 @@ $url = 'https://efactura.dgi.gub.uy:6443/ePrueba/ws_eprueba?wsdl';
 $localCert = '../assets/certificates/original.pem';
 try {
     $client = new SoapClient($url, ['local_cert' => $localCert, 'passphrase' => 'pass']);
-var_dump($client->__getFunctions());    
-exit;
     $xmlr = new SimpleXMLElement("<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:dgi='http://dgi.gub.uy'>
 <soapenv:Body>
    <dgi:WS_eFactura.EFACRECEPCIONSOBRE>
