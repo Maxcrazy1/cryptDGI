8a4e2639e60eff4b4ca6822e253eae3ee2647f2c
Cristian S <49521193+Maxcrazy1@users.noreply.github.com>
2021-01-29 08:26:00 -0400

    Create exportsql.bat

---
+++ exportsql.bat
@@ -0,0 +1,189 @@
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ABMGenerico.csv -Q "Select * from ABMGenerico;" -W -w 700 -s","
+
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ABMGenericoControles.csv -Q "Select * from ABMGenericoControles;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\Acciones.csv -Q "Select * from Acciones;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\admAplicaciones.csv -Q "Select * from admAplicaciones;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\admPermisos.csv -Q "Select * from admPermisos;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\admUsuarioNivel.csv -Q "Select * from admUsuarioNivel;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\admUsuarios.csv -Q "Select * from admUsuarios;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\Articulo.csv -Q "Select * from Articulo;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ArticuloCodigoBarras.csv -Q "Select * from ArticuloCodigoBarras;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ArticuloCodigoContacto.csv -Q "Select * from ArticuloCodigoContacto;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ArticuloCombo.csv -Q "Select * from ArticuloCombo;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ArticuloHayStock.csv -Q "Select * from ArticuloHayStock;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ArticuloVariedad.csv -Q "Select * from ArticuloVariedad;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ArticuloVariedadAtributo.csv -Q "Select * from ArticuloVariedadAtributo;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\AsignacionResguardo.csv -Q "Select * from AsignacionResguardo;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\CallesOriginales.csv -Q "Select * from CallesOriginales;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\CatImpuestosRecetas.csv -Q "Select * from CatImpuestosRecetas;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceAsientoCuenta.csv -Q "Select * from cceAsientoCuenta;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceAsientos.csv -Q "Select * from cceAsientos;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceAsientoCuenta.csv -Q "Select * from cceAsientoCuenta;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceAsientos.csv -Q "Select * from cceAsientos;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceBConciliacion.csv -Q "Select * from cceBConciliacion;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceBPlantillas.csv -Q "Select * from cceBPlantillas;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceBReferencias.csv -Q "Select * from cceBReferencias;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceComprobanteCuenta.csv -Q "Select * from cceComprobanteCuenta;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceComprobantePago.csv -Q "Select * from cceComprobantePago;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceComprobantes.csv -Q "Select * from cceComprobantes;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceCuentas.csv -Q "Select * from cceCuentas;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceCuentasDatosAdicionales.csv -Q "Select * from cceCuentasDatosAdicionales;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceEmpresas.csv -Q "Select * from cceEmpresas;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceGrupoCuenta.csv -Q "Select * from cceGrupoCuenta;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceParticipacionEmpresa.csv -Q "Select * from cceParticipacionEmpresa;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ccePresupuestos.csv -Q "Select * from ccePresupuestos;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceRatios.csv -Q "Select * from cceRatios;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceReferencias.csv -Q "Select * from cceReferencias;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\cceTerminosRatios.csv -Q "Select * from cceTerminosRatios;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\CierresCaja.csv -Q "Select * from CierresCaja;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\Comentarios.csv -Q "Select * from Comentarios;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ComprobanteArticulo.csv -Q "Select * from ComprobanteArticulo;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ComprobanteArticuloMemo.csv -Q "Select * from ComprobanteArticuloMemo;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ComprobanteNumeroSerie.csv -Q "Select * from ComprobanteNumeroSerie;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ComunicacionesCFE.csv -Q "Select * from ComunicacionesCFE;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ConteoBillete.csv -Q "Select * from ConteoBillete;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ControlDocumentos.csv -Q "Select * from ControlDocumentos;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ControlTickets.csv -Q "Select * from ControlTickets;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\Credenciales.csv -Q "Select * from Credenciales;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\Creditos.csv -Q "Select * from Creditos;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\eCAE.csv -Q "Select * from eCAE;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\eComprobantes.csv -Q "Select * from eComprobantes;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\Enlaces.csv -Q "Select * from Enlaces;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\EnvioBasico.csv -Q "Select * from EnvioBasico;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\eReportesDiarios.csv -Q "Select * from eReportesDiarios;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\eSobres.csv -Q "Select * from eSobres;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\Favoritos.csv -Q "Select * from Favoritos;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\FormulariosPersonalizados.csv -Q "Select * from FormulariosPersonalizados;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genAutonumerico.csv -Q "Select * from genAutonumerico;" -W -w 700 -s","
+
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genCodigoTexto.csv -Q "Select * from genCodigoTexto;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genCodigoTexto2.csv -Q "Select * from genCodigoTexto2;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genCotizacionesMoneda.csv -Q "Select * from genCotizacionesMoneda;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genDireccionesEnte.csv -Q "Select * from genDireccionesEnte;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genDirecciones.csv -Q "Select * from genDirecciones;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genEMails.csv -Q "Select * from genEMails;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genForaneas.csv -Q "Select * from genForaneas;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genFuentes.csv -Q "Select * from genFuentes;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genListas.csv -Q "Select * from genListas;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genMonedas.csv -Q "Select * from genMonedas;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genMovimientosFrecuentes.csv -Q "Select * from genMovimientosFrecuentes;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genParametros.csv -Q "Select * from genParametros;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genRecurrencias.csv -Q "Select * from genRecurrencias;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genRenglonMovFrecuentes.csv -Q "Select * from genRenglonMovFrecuentes;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genTelefonos.csv -Q "Select * from genTelefonos;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\genTipoCotizacionMoneda.csv -Q "Select * from genTipoCotizacionMoneda;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\hlpPermisos.csv -Q "Select * from hlpPermisos;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\Imagenes.csv -Q "Select * from Imagenes;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\MailCuenta.csv -Q "Select * from MailCuenta;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\MailDirecciones.csv -Q "Select * from MailDirecciones;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\MailErrors.csv -Q "Select * from MailErrors;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\MailPara.csv -Q "Select * from MailPara;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\MailReceptorEFactura.csv -Q "Select * from MailReceptorEFactura;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\Mails.csv -Q "Select * from Mails;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\NotNotificaciones.csv -Q "Select * from NotNotificaciones;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\NotNotificacionesUsuarios.csv -Q "Select * from NotNotificacionesUsuarios;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\Objetos.csv -Q "Select * from Objetos;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\PorcentadeDescuento.csv -Q "Select * from PorcentadeDescuento;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\Precios.csv -Q "Select * from Precios;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ProcesoCosteoManual.csv -Q "Select * from ProcesoCosteoManual;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ProcesoMP.csv -Q "Select * from ProcesoMP;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ProcesoPF.csv -Q "Select * from ProcesoPF;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\Procesos.csv -Q "Select * from Procesos;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\rc_Ordenes.csv -Q "Select * from rc_Ordenes;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\rc_TransaccionClienteNuevo.csv -Q "Select * from rc_TransaccionClienteNuevo;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\rc_Transacciones.csv -Q "Select * from rc_Transacciones;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\rc_TransaccionRenglon.csv -Q "Select * from rc_TransaccionRenglon;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\RecetaArticulo.csv -Q "Select * from RecetaArticulo;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\RecetasImpuestos.csv -Q "Select * from RecetasImpuestos;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\RecetaTasasImpuesto.csv -Q "Select * from RecetaTasasImpuesto;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\RelacionDocs.csv -Q "Select * from RelacionDocs;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\RelacionGrupos.csv -Q "Select * from RelacionGrupos;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\Reportes.csv -Q "Select * from Reportes;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ReportesCFG.csv -Q "Select * from ReportesCFG;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ReportesOnline.csv -Q "Select * from ReportesOnline;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\SaldosInicialesCC.csv -Q "Select * from SaldosInicialesCC;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\sisClasificacionComprobante.csv -Q "Select * from sisClasificacionComprobante;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\sisIconos.csv -Q "Select * from sisIconos;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\sisIconos.csv -Q "Select * from sisIconos;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\sisMenu.csv -Q "Select * from sisMenu;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\sisQueries.csv -Q "Select * from sisQueries;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\sisTipoCuenta.csv -Q "Select * from sisTipoCuenta;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\sisTipoReferencia.csv -Q "Select * from sisTipoReferencia;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\sisVentanasPanel.csv -Q "Select * from sisVentanasPanel;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\smsMensajes.csv -Q "Select * from smsMensajes;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\StockLocal.csv -Q "Select * from StockLocal;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\sysDeleteRowLog.csv -Q "Select * from sysDeleteRowLog;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\sysErrors.csv -Q "Select * from sysErrors;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\SysLogs.csv -Q "Select * from SysLogs;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\sysLogsMemo.csv -Q "Select * from sysLogsMemo;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\TipoArticulo.csv -Q "Select * from TipoArticulo;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\TipoComprobanteValores.csv -Q "Select * from TipoComprobanteValores;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\TipoDatoCampoAuxiliar.csv -Q "Select * from TipoDatoCampoAuxiliar;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\TipoDePrecio.csv -Q "Select * from TipoDePrecio;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\TipoEnvioZona.csv -Q "Select * from TipoEnvioZona;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\TiposDeObjetos.csv -Q "Select * from TiposDeObjetos;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\tpmClienteWeb.csv -Q "Select * from tpmClienteWeb;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\tpmDatosImportarArticulos.csv -Q "Select * from tpmDatosImportarArticulos;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\tpmDatosImportarComprobantes.csv -Q "Select * from tpmDatosImportarComprobantes;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\tpmDatosImportarComprobantesArticulos.csv -Q "Select * from tpmDatosImportarComprobantesArticulos;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\tpmDatosImportarContactos.csv -Q "Select * from tpmDatosImportarContactos;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\tmpDesglose.csv -Q "Select * from tmpDesglose;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\tmpImpEtiquetas.csv -Q "Select * from tmpImpEtiquetas;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\Traslados.csv -Q "Select * from Traslados;" -W -w 700 -s","
+sqlcmd -S PC-\SQLEXPRESS -d ZFactura1 -o c:\tablas-zureo\ValorCampoAuxiliar.csv -Q "Select * from ValorCampoAuxiliar;" -W -w 700 -s","
